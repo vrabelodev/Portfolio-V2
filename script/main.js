701336d654ada693c187150b806ab3de274fb507
Victor Rabelo <vrabeloqa@gmail.com>
2023-05-16 17:47:34 +0100

    Add navbar color change after scroll - Unfinished

--- script/main.js
+++ script/main.js
@@ -21,3 +21,17 @@ toggle.onclick = function () {
 };
 
 
+/** Navbar change color after scroll */
+const navbar = document.querySelector(".navbar");
+const scrollWatcher = document.createElement("div");
+
+scrollWatcher.setAttribute("data-scroll-watcher","");
+navbar.before(scrollWatcher);
+
+const navObserver = new IntersectionObserver(
+  (entries)=>{
+    navbar.classList.toggle("sticking")
+  }
+);
+
+navObserver.observe(scrollWatcher);
